17d60c44b5a46340fcbb6eeaee5378d6db4d7921
basil <basiln@icloud.com>
2026-06-04 12:34:49 +0300

    fix

---
+++ Week3/Day5/ExerciseXP/exercises.js
@@ -0,0 +1,47 @@
+//EXERCISE 1
+
+// #1.1
+// 3
+
+// #1.2
+// Error - const cannot be reassigned
+
+// #2.1
+// 0
+// 5
+
+// #2.2
+// Error - const cannot be reassigned
+
+// #3.1
+// hello
+
+// #4.1
+// test
+
+// #4.2
+// test
+
+// #5.1
+// 5
+// 2
+
+// #5.2
+// 5
+// 2
+
+//EXERCISE 2
+
+winBattle = () => {
+  return true;
+};
+
+var experiencePoints = winBattle
+  ? (experiencePoints = 10)
+  : (experiencePoints = 1);
+
+console.log(experiencePoints);
+
+
+//EXERCISE 3
+
